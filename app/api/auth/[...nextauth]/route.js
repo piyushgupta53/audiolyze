388e571779b5323fc06b4afd44e371d7d1b89887
Piyush Gupta <PIYUSHGUPTA53@GMAIL.COM>
2023-11-26 23:26:12 +0530

    Update route.js
    changes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -13,7 +13,7 @@ const handler = NextAuth({
         },
       },
       // Explicitly define the callback URL
-      callbackUrl: `${process.env.NEXTAUTH_URL}/api/auth/callback/spotify`,
+      callbackUrl: `${process.env.NEXTAUTH_URL}/dashboard`,
     }),
   ],
   callbacks: {
